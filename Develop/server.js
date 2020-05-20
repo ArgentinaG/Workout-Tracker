23327f7aefa712f6b4a0745ac92d1ae1b2305e2d
ArgentinaG <59855898+ArgentinaG@users.noreply.github.com>
2020-05-19 23:38:55 -0500

    establised routes

--- Develop/server.js
+++ Develop/server.js
@@ -6,17 +6,18 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Connect to the Mongo DB
+mongoose.connect(MONGODB_URI);
+
 app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-
-// Connect to the Mongo DB
-mongoose.connect(MONGODB_URI);
 
 //routes
 require('./routes/api-routes')(app)
