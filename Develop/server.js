47e85b5f5c4e9931faae7c37640db02d32e4e813
ArgentinaG <59855898+ArgentinaG@users.noreply.github.com>
2020-05-19 23:18:00 -0500

    created heroku file

---
+++ Develop/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Connect to the Mongo DB
+mongoose.connect(MONGODB_URI);
+
+//routes
+require('./routes/api-routes')(app)
+require('./routes/html-routes')(app)
+
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}..`);
+})
